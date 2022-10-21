4b78c3979aaf197f3f20e92a835a978a5768aca9
radioActive DROID <me@godwin.dev>
2022-10-21 01:36:41 +0100

    Added new fonts

--- src/Mobile/MauiProgram.cs
+++ src/Mobile/MauiProgram.cs
@@ -19,6 +19,10 @@ public static class MauiProgram {
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Roboto-Thin.ttf", "RobotoThin");
+                fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
+                fonts.AddFont("Roboto-Medium.ttf", "RobotoMedium");
+                fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
                 fonts.AddFontAwesomeIconFonts();
             });
         builder.Services.AddSingleton<App>();
