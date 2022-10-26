d4bedcc6b4470d09285a3ed452ef3c1ad8b04729
radioActive DROID <me@godwin.dev>
2022-10-26 19:04:56 +0100

    Added icons constant class

---
+++ src/Mobile/Models/Icons.cs
@@ -0,0 +1,13 @@
+﻿namespace XClaim.Mobile.Models
+{
+    internal static class Icons
+    {
+        public const string Apple = "apple_icon.svg";
+        public const string Google = "google_icon.svg";
+        public const string Claim = "icon_claim.svg";
+        public const string Home = "icon_home.svg";
+        public const string Payment = "icon_payment.svg";
+        public const string AuthBanner = "auth_vector.svg";
+        public const string WelcomeBanner = "welcome_vector.png";
+    }
+}
