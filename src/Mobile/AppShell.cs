43db2d4c24bbd40fe24890c4e3c82be4824ab978
radioActive DROID <me@godwin.dev>
2022-10-27 16:08:37 +0100

    Added review page to shell

--- src/Mobile/AppShell.cs
+++ src/Mobile/AppShell.cs
@@ -3,6 +3,8 @@ using XClaim.Mobile.Pages.Claim;
 using XClaim.Mobile.Pages.Home;
 using XClaim.Mobile.Pages.Payment;
 using XClaim.Mobile.Pages.Profile;
+using XClaim.Mobile.Pages.Review;
+using XClaim.Mobile.Pages.Startup;
 
 namespace XClaim.Mobile;
 
@@ -10,51 +12,83 @@ public class AppShell : Shell
 {
 	public AppShell() {
         FlyoutBehavior = FlyoutBehavior.Disabled;
+        BindingContext = new AppShellViewModel();
+
         Items.Add(new ShellContent
         {
+            FlyoutItemIsVisible = false,
+            Route = nameof(LoadingPage),
+            ContentTemplate = new DataTemplate(typeof(LoadingPage))
+        });
+
+        Items.Add(new ShellContent {
             FlyoutItemIsVisible = false,
             Route = nameof(AuthPage),
             ContentTemplate = new DataTemplate(typeof(AuthPage))
         });
-        Items.Add(new TabBar {
+
+        var TabItems = new TabBar {
             Items = {
             new Tab {
                 Title = "Home",
                 Icon = Icons.Home,
                 Items = {
-                        new ShellContent  {
-                            Route = nameof(HomePage),
-                            ContentTemplate = new DataTemplate(typeof(HomePage))
-                        }
+                    new ShellContent {
+                        Route = nameof(HomePage),
+                        ContentTemplate = new DataTemplate(typeof(HomePage))
+                    }
                  }
             },
             new Tab {
                 Title = "Claims",
                 Icon = Icons.Claim,
                 Items = {
-                        new ShellContent  {
-                            Route = nameof(ClaimPage),
-                            ContentTemplate = new DataTemplate(typeof(ClaimPage))
-                        }
+                    new ShellContent {
+                        Route = nameof(ClaimPage),
+                        ContentTemplate = new DataTemplate(typeof(ClaimPage))
+                    }
                  }
             },
             new Tab {
                 Title = "Payments",
                 Icon = Icons.Payment,
                 Items = {
-                        new ShellContent {
-                            Route = nameof(PaymentPage),
-                            ContentTemplate = new DataTemplate(typeof(PaymentPage))
-                        }
-                 }
+                    new ShellContent {
+                        Route = nameof(PaymentPage),
+                        ContentTemplate = new DataTemplate(typeof(PaymentPage))
+                    }
+                }
             }
          }
+        };
+
+        TabItems.Items.Add(new Tab {
+            Title = "Review",
+            Icon = Icons.Review,
+            Items = {
+                new ShellContent {
+                    Route = nameof(ReviewPage),
+                    ContentTemplate = new DataTemplate(typeof(ReviewPage))
+                }
+            }
         });
-        Items.Add(new ShellContent
-        {
+
+        Items.Add(TabItems);
+
+        Items.Add(new ShellContent {
+            FlyoutItemIsVisible = false,
+            Route = nameof(NotificationPage),
+            ContentTemplate = new DataTemplate(typeof(NotificationPage))
+        });
+
+        Items.Add(new ShellContent {
             FlyoutItemIsVisible = false,
             Route = nameof(ProfilePage),
             ContentTemplate = new DataTemplate(typeof(ProfilePage))
         });
     }
+}
+
+public class AppShellViewModel : BaseViewModel {
+    async void SignOut() { }
 }
--- src/Mobile/Models/Icons.cs
+++ src/Mobile/Models/Icons.cs
@@ -1,14 +1,14 @@
-﻿namespace XClaim.Mobile.Models
+﻿namespace XClaim.Mobile.Models;
+
+internal static class Icons
 {
-    internal static class Icons
-    {
-        public const string Apple = "apple_icon.svg";
-        public const string Google = "google_icon.svg";
-        public const string Claim = "icon_claim.svg";
-        public const string ChevronLeft = "chevron_left_solid.svg";
-        public const string Home = "icon_home.svg";
-        public const string Payment = "icon_payment.svg";
-        public const string AuthBanner = "auth_vector.svg";
-        public const string WelcomeBanner = "welcome_vector.png";
-    }
+    public const string Apple = "apple_icon.svg";
+    public const string Google = "google_icon.svg";
+    public const string Claim = "icon_claim.svg";
+    public const string ChevronLeft = "chevron_left_solid.svg";
+    public const string Review = "icon_review.svg";
+    public const string Home = "icon_home.svg";
+    public const string Payment = "icon_payment.svg";
+    public const string AuthBanner = "auth_vector.svg";
+    public const string WelcomeBanner = "welcome_vector.png";
 }
