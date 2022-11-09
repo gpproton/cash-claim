8d8dbc9290d5cd851adb79513b1a5e1170e36bf3
radioActive DROID <me@godwin.dev>
2022-11-10 00:49:25 +0100

    Create ThemeColors.cs

---
+++ src/Mobile/Resources/Styles/ThemeColors.cs
@@ -0,0 +1,37 @@
+﻿namespace XClaim.Mobile.Resources.Styles;
+
+internal static class ThemeColors {
+    // Light mode colors
+    public static Color Primary { get; set; } = Color.Parse("#4E549C");
+    public static Color Secondary { get; set; } = Color.Parse("#919AFA");
+    public static Color Accent { get; set; } = Color.Parse("#919AFA");
+    public static Color Tertiary { get; set; } = Color.Parse("#EBECF9");
+    public static Color Neutral { get; set; } = Color.Parse("#969696");
+    public static Color Error { get; set; } = Color.Parse("#C00");
+    public static Color Warning { get; set; } = Color.Parse("#FFCC00");
+
+    // Dark mode colors
+    public static Color BackgroundDark { get; set; } = Color.Parse("#081B25");
+    public static Color BackgroundMid { get; set; } = Color.Parse("#152C39");
+    public static Color DarkGray { get; set; } = Color.Parse("#212125");
+    public static Color MidGray { get; set; } = Color.Parse("#CFCFD3");
+    public static Color LightGray { get; set; } = Color.Parse("#EFEFEF");
+
+    // Standard colors
+    public static Color Gray100 { get; set; } = Color.Parse("#E1E1E1");
+    public static Color Gray200 { get; set; } = Color.Parse("#C8C8C8");
+    public static Color Gray300 { get; set; } = Color.Parse("#ACACAC");
+    public static Color Gray400 { get; set; } = Color.Parse("#919191");
+    public static Color Gray500 { get; set; } = Color.Parse("#6E6E6E");
+    public static Color Gray600 { get; set; } = Color.Parse("#404040");
+    public static Color Gray900 { get; set; } = Color.Parse("#212121");
+    public static Color Gray950 { get; set; } = Color.Parse("#141414");
+
+    // Typography colors
+    public static Color TextLight { get; set; } = Color.Parse("#000000");
+    public static Color TextDark { get; set; } = Color.Parse("#FFFFFF");
+    public static Color TextOnSolidLight { get; set; } = Color.Parse("#FFFFFF");
+    public static Color TextOnSolidDark { get; set; } = Color.Parse("#000000");
+    public static Color TextSecondaryLight { get; set; } = Color.Parse("#1d1d1d");
+    public static Color TextSecondaryDark { get; set; } = Color.Parse("#f1f1f1");
+}
