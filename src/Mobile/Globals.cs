f6a89a716cc992f9c2b938c3794025156d60ab24
radioActive DROID <me@godwin.dev>
2022-12-23 21:46:34 +0100

    Update Globals.cs

--- src/Mobile/Globals.cs
+++ src/Mobile/Globals.cs
@@ -5,6 +5,7 @@ global using CommunityToolkit.Maui.Markup;
 global using CommunityToolkit.Maui.Views;
 global using static CommunityToolkit.Maui.Markup.GridRowsColumns;
 global using FA = UraniumUI.Icons.FontAwesome;
+global using UraniumUI.Material.Controls;
 global using BindableProps;
 global using XClaim.Mobile.Handlers;
 global using XClaim.Mobile.Models;
