597b0d2b9c846631dbd289d9e64a69305d77d95e
radioActive DROID <me@godwin.dev>
2022-12-28 23:56:08 +0100

    Update Comment.cs

--- src/Common/Entities/Comment.cs
+++ src/Common/Entities/Comment.cs
@@ -2,7 +2,7 @@
 
 namespace XClaim.Common.Entities;
 
-internal class Comment : BaseEntity {
+public class Comment : BaseEntity {
     public Claim? Claim { get; set; }
     public Payment? Payment { get; set; }
     public User User { get; set; }
