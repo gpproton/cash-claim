9f615fa669745a3023f5f04949b524747fd5f0da
radioActive DROID <me@godwin.dev>
2022-12-28 13:45:32 +0100

    Updated review list screen

--- src/Mobile/Views/Review/ReviewView.cs
+++ src/Mobile/Views/Review/ReviewView.cs
@@ -17,78 +17,79 @@ public class ReviewView : BaseView<ReviewViewModel> {
 
     protected override void Build() {
         Title = "Reviews";
-        Content = new VerticalStackLayout {
+        Content = new Grid {
+            RowDefinitions = Rows.Define(
+                (SectionLevel.First, Star)
+            ),
             Children = {
-                new ScrollView {
-                      Content = Content = new CollectionView() { SelectionMode = SelectionMode.None }
-                     .EmptyViewTemplate(new DataTemplate(() =>  new EmptyItemView().Margins(0, 56)))
-                     .Bind(ItemsView.ItemsSourceProperty, nameof(ReviewViewModel.Items))
-                     .Bind(SelectableItemsView.SelectedItemProperty, nameof(ReviewViewModel.Selected))
-                     .ItemTemplate(new DataTemplate(() => new Grid {
-                        ColumnDefinitions = Columns.Define(
-                            (SectionLevel.First, Auto),
-                            (SectionLevel.Second, Star),
-                            (SectionLevel.Third, Auto)
-                        ),
-                        RowDefinitions = Rows.Define(
-                            (SectionLevel.First, Auto),
-                            (SectionLevel.Second, Auto),
-                            (SectionLevel.Third, 1)
-                        ),
+                new CollectionView() {
+                    SelectionMode = SelectionMode.None,
+                    EmptyView = "No items to display"
+                }.Bind(ItemsView.ItemsSourceProperty, nameof(ReviewViewModel.Items))
+                .ItemTemplate(new DataTemplate(() => new Grid {
+                ColumnDefinitions = Columns.Define(
+                    (SectionLevel.First, Auto),
+                    (SectionLevel.Second, Star),
+                    (SectionLevel.Third, Auto)
+                ),
+                RowDefinitions = Rows.Define(
+                    (SectionLevel.First, Auto),
+                    (SectionLevel.Second, Auto),
+                    (SectionLevel.Third, 1)
+                ),
+                Children = {
+                    new AvatarView() {
+                    CornerRadius = 8,
+                    Margin = 4,
+                    Content = new Image().Source(new FontImageSource() {
+                    FontFamily = "FASolid",
+                    Glyph = FA.Solid.Book,
+                    Color = Colors.Indigo
+                    }).Size(24)
+                }.BackgroundColor(Colors.LightGray)
+                .Size(48, 48)
+                .Column(SectionLevel.First)
+                .RowSpan(2),
+                    new Label { TextColor = Color.FromRgba("#7F7F7F") }
+                    .Font(size: 14)
+                    .Bind(Label.TextProperty, nameof(ReviewDto.Name))
+                    .Row(SectionLevel.First)
+                    .Column(SectionLevel.Second)
+                    .Margins(0, 8, 0, 0),
+                    new HorizontalStackLayout {
                         Children = {
-                          new AvatarView() {
-                            CornerRadius = 8,
-                            Margin = 4,
-                            Content = new Image().Source(new FontImageSource() {
-                            FontFamily = "FASolid",
-                            Glyph = FA.Solid.Book,
-                            Color = Colors.Indigo
-                            }).Size(24)
-                        }.BackgroundColor(Colors.LightGray)
-                        .Size(48, 48)
-                        .Column(SectionLevel.First)
-                        .RowSpan(2),
-                         new Label { TextColor = Color.FromRgba("#7F7F7F") }
-                            .Font(size: 14)
-                            .Bind(Label.TextProperty, nameof(ReviewDto.Name))
-                            .Row(SectionLevel.First)
-                            .Column(SectionLevel.Second)
-                            .Margins(0, 8, 0, 0),
-                          new HorizontalStackLayout {
-                              Children = {
-                                 new Label()
-                                .Font(size: 11)
-                                .Bind(Label.TextProperty, nameof(ReviewDto.Time), convert: (DateTime value) => value.ToString("dd-MMM-yyyy")),
-                                new Label()
-                                .Font(size: 11)
-                                .Text(".")
-                                .Margins(3, 0, 3, 0),
-                                 new Label()
-                                 .Font(size: 11)
-                                .Bind(Label.TextProperty, nameof(ReviewDto.Owner))
-                              }
-                          }.Row(SectionLevel.Second)
-                          .Column(SectionLevel.Second),
-                         new Label { TextColor = Colors.Gray }
-                            .Font(size: 18)
-                            .Bind(Label.TextProperty, nameof(ReviewDto.Amount), convert: (decimal value) => "₦" + string.Format("{0:N0}", value))
-                            .MinWidth(95)
-                            .Row(SectionLevel.First)
-                            .Column(SectionLevel.Third)
-                            .CenterVertical()
-                            .CenterHorizontal(),
-                         new Label()
+                            new Label()
+                        .Font(size: 11)
+                        .Bind(Label.TextProperty, nameof(ReviewDto.Time), convert: (DateTime value) => value.ToString("dd-MMM-yyyy")),
+                        new Label()
+                        .Font(size: 11)
+                        .Text(".")
+                        .Margins(3, 0, 3, 0),
+                            new Label()
                             .Font(size: 11)
-                            .Bind(Label.TextProperty, nameof(ReviewDto.Status))
-                            .Row(SectionLevel.Second)
-                            .Column(SectionLevel.Third),
-                         new BoxView()
-                             .DynamicResource(StyleProperty, "SeparatorLine")
-                             .Row(SectionLevel.Third)
-                             .ColumnSpan(3)
+                        .Bind(Label.TextProperty, nameof(ReviewDto.Owner))
                         }
-                     }.Paddings(4, 8, 4, 4)))
-                    }.FillVertical()
+                    }.Row(SectionLevel.Second)
+                    .Column(SectionLevel.Second),
+                    new Label { TextColor = Colors.Gray }
+                    .Font(size: 18)
+                    .Bind(Label.TextProperty, nameof(ReviewDto.Amount), convert: (decimal value) => "₦" + string.Format("{0:N0}", value))
+                    .MinWidth(95)
+                    .Row(SectionLevel.First)
+                    .Column(SectionLevel.Third)
+                    .CenterVertical()
+                    .CenterHorizontal(),
+                    new Label()
+                    .Font(size: 11)
+                    .Bind(Label.TextProperty, nameof(ReviewDto.Status))
+                    .Row(SectionLevel.Second)
+                    .Column(SectionLevel.Third),
+                    new BoxView()
+                        .DynamicResource(StyleProperty, "SeparatorLine")
+                        .Row(SectionLevel.Third)
+                        .ColumnSpan(3)
+                }
+              }.Paddings(4, 8, 4, 4)))
             }
         };
     }
