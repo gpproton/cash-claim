1ad022211fb1ce63dd265d9720ed792b1d3be5a4
radioActive DROID <me@godwin.dev>
2022-12-30 12:13:46 +0100

    Added bank form

--- src/Mobile/Views/Profile/BankView.cs
+++ src/Mobile/Views/Profile/BankView.cs
@@ -1,14 +1,56 @@
 namespace XClaim.Mobile.Views.Profile;
 
 public class BankView : BaseView<BankViewModel> {
+
+    private enum SectionLevel {
+        First,
+        Second,
+        Third
+    }
+
     public BankView(BankViewModel vm) : base(vm) => Build();
 
     protected override void Build() {
         Title = "Bank Account";
-        Content = new VerticalStackLayout {
-             new Label().Text("Bank!!").CenterHorizontal()
+        Content = new Grid {
+            Padding = 8,
+            RowDefinitions = Rows.Define(
+                (SectionLevel.First, Star),
+                (SectionLevel.Second, Auto)
+            ),
+            Children = {
+                new VerticalStackLayout {
+                    Spacing = 10,
+                    Children = {
+                        new TextField { Title = "Account Name" },
+                        new TextField { Title = "Account Number", Keyboard = Keyboard.Numeric },
+                        new PickerField { Title = "Bank", AllowClear = false }
+                            .Bind(PickerField.ItemsSourceProperty, nameof(BankViewModel.Banks)),
+                        new Editor { Placeholder = "Description" }
+                            .Height(120)
+                    }
+                }.CenterHorizontal()
+                .FillHorizontal()
+                .Row(SectionLevel.First),
+                 new Button().Text("Save")
+                    .DynamicResource(StyleProperty, "ButtonLargePrimary")
+                    .CenterVertical()
+                    .Row(SectionLevel.Second)
+            }
         };
+    }
+
+    protected override void OnAppearing() {
+        base.OnAppearing();
+        BindingContext.LoadCommand.Execute(null);
     }
 }
 
-public partial class BankViewModel : BaseViewModel { }
+public partial class BankViewModel : BaseViewModel {
+    [ObservableProperty] private ObservableCollection<string> _banks;
+
+    [RelayCommand]
+    private void Load() {
+        Banks = new ObservableCollection<string>() { "GT Bank", "Zenith", "Union Bank" };
+    }
+}
