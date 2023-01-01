616e04d7ba50e246edac061fcb796ea719eb7d87
radioActive DROID <me@godwin.dev>
2023-01-01 01:20:35 +0100

    Added MudBlazor to web client projects

--- src/Web/Client/Program.cs
+++ src/Web/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using XClaim.Web.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
