3084d842ce9167f1dd65981398bbb2361c3065a5
radioActive DROID <me@godwin.dev>
2023-01-04 06:51:05 +0100

    Create DataConst.cs

---
+++ src/Mobile/DataConst.cs
@@ -0,0 +1,17 @@
+﻿namespace XClaim.Mobile;
+
+public static class DataConst {
+    public const string DatabaseFilename = "app-data.db3";
+
+    public const SQLite.SQLiteOpenFlags Flags =
+        SQLite.SQLiteOpenFlags.ReadWrite |
+        SQLite.SQLiteOpenFlags.Create |
+        SQLite.SQLiteOpenFlags.SharedCache;
+
+    public static string DatabasePath {
+        get {
+            var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            return Path.Combine(basePath, DatabaseFilename);
+        }
+    }
+}
