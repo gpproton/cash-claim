841c0212c636de1a9a5200e20f6935eb1f8a4f80
radioActive DROID <me@godwin.dev>
2023-01-04 20:31:16 +0100

    Create UserDto.cs

---
+++ src/Common/Dtos/UserDto.cs
@@ -0,0 +1,5 @@
+﻿using XClaim.Common.Enums;
+
+namespace XClaim.Common.Dtos;
+
+public record UserDto (string Email, string Phone, string FirstName, string LastName, decimal Balance, UserPermission Permission, bool Verified);
