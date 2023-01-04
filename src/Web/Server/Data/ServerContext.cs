665ea5d6a57c511a45c53fbea25c31f8340ad17a
radioActive DROID <me@godwin.dev>
2023-01-04 20:32:34 +0100

    Added db context

---
+++ src/Web/Server/Data/ServerContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using XClaim.Common.Entities;
+
+namespace XClaim.Web.Server.Data;
+
+public class ServerContext : DbContext {
+    public ServerContext(DbContextOptions<ServerContext> options) : base(options) { }
+
+    public DbSet<UserEntity> Users { get; set; } = default!;
+    public DbSet<TeamEntity> Teams { get; set; } = default!;
+    public DbSet<BankEntity> Banks { get; set; } = default!;
+    public DbSet<BankAccountEntity> BankAccounts { get; set; } = default!;
+    public DbSet<CategoryEntity> Categories { get; set; } = default!;
+    public DbSet<CommentEntity> Comments { get; set; } = default!;
+    public DbSet<EventEntity> Events { get; set; } = default!;
+    public DbSet<ClaimEntity> Claims { get; set; } = default!;
+    public DbSet<FileEntity> Files { get; set; } = default!;
+    public DbSet<PaymentEntity> Payments { get; set; } = default!;
+}
