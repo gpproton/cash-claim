e5fa6a55bdd834039fe98e2ffc0db4c8dc9a6c84
radioActive DROID <me@godwin.dev>
2023-01-05 15:47:41 +0100

    lint record

--- src/Common/Dtos/UserDto.cs
+++ src/Common/Dtos/UserDto.cs
@@ -2,4 +2,4 @@
 
 namespace XClaim.Common.Dtos;
 
-public record UserDto (string Email, string Phone, string FirstName, string LastName, decimal Balance, UserPermission Permission, bool Verified);
+public record UserDto(string Email, string Phone, string FirstName, string LastName, decimal Balance, UserPermission Permission, bool Verified);
