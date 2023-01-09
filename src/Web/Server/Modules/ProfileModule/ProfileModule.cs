e03b9a01645ee8941d6c8f5c0b143f073e50aa21
radioActive DROID <me@godwin.dev>
2023-01-09 04:41:59 +0100

    improve profile API

--- src/Web/Server/Modules/ProfileModule/ProfileModule.cs
+++ src/Web/Server/Modules/ProfileModule/ProfileModule.cs
@@ -1,10 +1,12 @@
 using System.Security.Claims;
+using Newtonsoft.Json;
 using XClaim.Common.Dtos;
-using XClaim.Common.Enums;
 
 namespace XClaim.Web.Server.Modules.ProfileModule;
 
 public class ProfileModule : IModule {
+    record ProfileResponse(bool Valid = false, string Message = "", Profile? Data = null);
+    
     public IServiceCollection RegisterApiModule(IServiceCollection services) {
 
         return services;
@@ -15,20 +17,26 @@ public class ProfileModule : IModule {
         var url = $"{Constants.RootApi}/{name.ToLower()}";
         var group = endpoints.MapGroup(url).WithTags(name);
 
-        group.MapGet("/account", (context) => {
-        bool isAuth = context.User.Identity?.IsAuthenticated ?? false;
-        if (!isAuth) return context.Response.WriteAsync("None");
-            
+        group.MapGet("/account", async (context) => {
+            bool isAuth = context.User.Identity?.IsAuthenticated ?? false;
+        if (!isAuth) await context.Response.WriteAsJsonAsync(
+            new ProfileResponse(false, "Not Authenticated", null)
+            );
+        
         var email = context.User.FindFirst(ClaimTypes.Email)?.Value;
         var fullName = context.User.FindFirst(ClaimTypes.Name)?.Value;
         var phone = context.User.FindFirst(ClaimTypes.MobilePhone)?.Value;
 
-        if (email is null) {
-            context.Response.Redirect("/");
-        }
-        
-        return context.Response.WriteAsJsonAsync(new Profile(email!, fullName, phone, UserPermission.Standard));
-        
+        if (email is null)
+            await context.Response.WriteAsJsonAsync(
+                new ProfileResponse(false, "Email address is invalid", null)
+                );
+
+        var profile = new Profile(email!, fullName!, phone!);
+        // context.Response.StatusCode = 200;
+        await context.Response.WriteAsJsonAsync(
+            new ProfileResponse(false, "Success", profile)
+            );
         }).WithName("AccountProfile").WithOpenApi();
 
         return group;
