a178c0e2ece639cc6991b55bb64ff069c6cb5e29
radioActive DROID <me@godwin.dev>
2023-01-11 06:35:23 +0100

    Update CurrencyEntity.cs

--- src/Common/Entities/CurrencyEntity.cs
+++ src/Common/Entities/CurrencyEntity.cs
@@ -3,7 +3,7 @@ using XClaim.Common.Base;
 namespace XClaim.Common.Entities;
 
 public class CurrencyEntity : BaseEntity {
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
-    public string Symbol { get; set; }
+    public string Symbol { get; set; } = string.Empty;
 }
