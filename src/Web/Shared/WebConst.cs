b782bbc1a9746eac1a69d689f077ab47eb1bfc4b
radioActive DROID <me@godwin.dev>
2023-01-13 15:05:35 +0100

    update Web constants

---
+++ src/Web/Shared/WebConst.cs
@@ -0,0 +1,10 @@
+namespace XClaim.Web.Shared;
+
+public static class WebConst {
+    public const string RootApi = "/api/v1";
+    public const string RootUri = "https://localhost:7001";
+    public const string ApiAuth = "auth/sign-in";
+    public const string AppHome = "app/overview";
+    public const string AppAuth = "app/auth";
+    public const string AppRegister = "app/registration";
+}
--- src/Web/Shared/WebConstants.cs
+++
@@ -1,6 +0,0 @@
-namespace XClaim.Web.Shared;
-
-public static class WebConstants {
-    public const string RootApi = "/api/v1";
-    public const string RedirectAddress = "";
-}
