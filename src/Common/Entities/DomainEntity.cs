2a9793c1ab90d6954537c3f0aaa209f5fa2f4a6d
radioActive DROID <me@godwin.dev>
2023-01-15 22:43:40 +0100

    Updated domain page

--- src/Common/Entities/DomainEntity.cs
+++ src/Common/Entities/DomainEntity.cs
@@ -4,4 +4,6 @@ namespace XClaim.Common.Entities;
 
 public class DomainEntity : BaseEntity {
     public string Address { get; set; } = string.Empty;
+    public bool Active { get; set; }
+    public string Description { get; set; } = string.Empty;
 }
