65a5f4211c4f5870aaa0e21d45d86cb3ddb1f11e
radioActive DROID <me@godwin.dev>
2023-01-15 23:06:02 +0100

    Added result to teams page

--- src/Common/Entities/TeamEntity.cs
+++ src/Common/Entities/TeamEntity.cs
@@ -6,9 +6,11 @@ namespace XClaim.Common.Entities;
 public sealed class TeamEntity : BaseEntity {
     [MaxLength(128)]
     public string Name { get; set; } = string.Empty;
+    public bool Active { get; set; }
+    public string Description { get; set; } = string.Empty;
     public CompanyEntity? Company { get; set; }
     public Guid? CompanyId { get; set; }
     public UserEntity? Manager { get; set; }
     public Guid? ManagerId { get; set; }
-    public ICollection<UserEntity>? Members { get; set; } = default!;
+    public ICollection<UserEntity> Members { get; set; } = default!;
 }
