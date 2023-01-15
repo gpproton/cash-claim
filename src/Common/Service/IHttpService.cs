7733050d42197c7789b1231d7abe7af6ddaa3a65
radioActive DROID <me@godwin.dev>
2023-01-15 01:54:29 +0100

    Implemented abstract HTTP service

---
+++ src/Common/Service/IHttpService.cs
@@ -0,0 +1,11 @@
+namespace XClaim.Common.Service;
+
+public interface IHttpService {
+    Task<T> Get<T>(string uri);
+    Task Post(string uri, object? value);
+    Task<T> Post<T>(string uri, object? value);
+    Task Put(string uri, object? value);
+    Task<T> Put<T>(string uri, object? value);
+    Task Delete(string uri);
+    Task<T> Delete<T>(string uri);
+}
