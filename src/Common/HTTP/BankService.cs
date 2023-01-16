33ed39827d4ee609231f24e7dfe8983069bfe903
radioActive DROID <me@godwin.dev>
2023-01-16 09:49:10 +0100

    Implement simple API mutation

--- src/Common/HTTP/BankService.cs
+++ src/Common/HTTP/BankService.cs
@@ -12,4 +12,16 @@ public class BankService : IBankService {
     public async Task<List<BankResponse>> GetAllAsync() {
         return await _http.Get<List<BankResponse>>("api/v1/bank" + "?Page=1&PerPage=25&SortBy=Ascending&CombineWith=Or");
     }
+    public async Task<BankResponse> GetByIdAsync(Guid id) {
+        return await _http.Get<BankResponse>($"api/v1/bank/{id}");
+    }
+    public async Task<BankResponse> CreateAsync(BankResponse bank) {
+        return await _http.Post<BankResponse>($"api/v1/bank", bank);
+    }
+    public async Task<BankResponse> UpdateAsync(BankResponse bank) {
+        return await _http.Put<BankResponse>($"api/v1/bank", bank);
+    }
+    public async Task<BankResponse> ArchiveAsync(Guid id) {
+        return await _http.Delete<BankResponse>($"api/v1/bank/{id}");
+    }
 }
--- src/Common/HTTP/IBankService.cs
+++ src/Common/HTTP/IBankService.cs
@@ -4,4 +4,13 @@ namespace XClaim.Common.HTTP;
 
 public interface IBankService {
     Task<List<BankResponse>> GetAllAsync();
+
+    Task<BankResponse> GetByIdAsync(Guid id);
+    
+    Task<BankResponse> CreateAsync(BankResponse bank);
+    
+    Task<BankResponse> UpdateAsync(BankResponse bank);
+    
+    Task<BankResponse> ArchiveAsync(Guid id);
+    
 }
