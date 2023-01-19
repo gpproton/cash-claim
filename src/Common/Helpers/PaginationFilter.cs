0074b3f6af5fd0a5ec0b4865933373b52664650a
radioActive DROID <me@godwin.dev>
2023-01-19 08:54:47 +0100

    Update PaginationFilter.cs

--- src/Common/Helpers/PaginationFilter.cs
+++ src/Common/Helpers/PaginationFilter.cs
@@ -1,6 +1,6 @@
 namespace XClaim.Common.Helpers;
 
-public class PaginationHelper {
+public class PaginationFilter {
     public int Page { get; set; } = 1;
     public int PerPage { get; set; } = 25;
     public string SortBy { get; set; } = "Ascending";
