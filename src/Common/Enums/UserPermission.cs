206f63d217d79934c089fa0d59101e38429b0987
radioActive DROID <me@godwin.dev>
2023-02-01 05:11:36 +0100

    Added descriptions to enums

--- src/Common/Enums/UserPermission.cs
+++ src/Common/Enums/UserPermission.cs
@@ -1,10 +1,14 @@
-﻿namespace XClaim.Common.Enums;
+﻿using System.ComponentModel;
+
+namespace XClaim.Common.Enums;
 
 public enum UserPermission {
     System,
     Administrator,
+    [Description("Finance Manager")]
     Finance,
     Cashier,
+    [Description("Department Head")]
     Lead,
     Standard
 }
