0709d2042337bbe5b0dd0a93ec14bcf8c9a93e9d
radioActive DROID <me@godwin.dev>
2023-02-02 02:26:40 +0100

    Update event service

--- src/Web/Server/Modules/EventModule/IEventService.cs
+++ src/Web/Server/Modules/EventModule/IEventService.cs
@@ -1,5 +1,11 @@
+using XClaim.Common.Dtos;
+using XClaim.Common.Wrappers;
+
 namespace XClaim.Web.Server.Modules.EventModule;
 
 public interface IEventService {
-    Task ClearOld();
+    Task<PagedResponse<List<EventResponse>>> GetRecentAsync();
+    Task<PagedResponse<List<EventResponse>>> GetHistoryAsync(EventFilter filter);
+    Task<PagedResponse<List<EventResponse>>> GetAccountAuditAsync();
+    Task<Response<bool>> ClearOld();
 }
