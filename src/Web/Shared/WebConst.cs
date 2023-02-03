3a6c83ddb3872c79e231fff6542ab0704496f04d
radioActive DROID <me@godwin.dev>
2023-02-03 16:19:24 +0100

    Adjust tables height

--- src/Web/Shared/WebConst.cs
+++ src/Web/Shared/WebConst.cs
@@ -11,6 +11,6 @@ public static class WebConst {
     public const string AppRegister = "app/registration";
     public const string SessionKey = "UserSession";
     public static readonly DateRange AppDateRange = new DateRange(DateTime.Now.AddDays(-7), DateTime.Now);
-    public static readonly int[] AppPaged = new[] { 25, 100, 250, 2000 };
-    public const string TableHeight = "calc(100vh - 256px)";
+    public static readonly int[] AppPaged = { 25, 100, 250, 2000 };
+    public const string TableHeight = "calc(100vh - 305px)";
 }
