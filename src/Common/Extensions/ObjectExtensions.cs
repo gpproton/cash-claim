639b050cbd75de0c4268a7fbd672220a29a73c97
radioActive DROID <me@godwin.dev>
2023-02-07 17:38:10 +0100

    Update ObjectExtensions.cs

--- src/Common/Extensions/ObjectExtensions.cs
+++ src/Common/Extensions/ObjectExtensions.cs
@@ -7,7 +7,7 @@ public static class ObjectExtensions {
         if (obj == null) return string.Empty;
         var properties = from p in obj.GetType().GetProperties()
                          where p.GetValue(obj, null) != null
-                         select p.Name + "=" + HttpUtility.UrlEncode(p.GetValue(obj, null).ToString());
+                         select p.Name + "=" + HttpUtility.UrlEncode(p!.GetValue(obj, null).ToString());
 
         return "?" + String.Join("&", properties.ToArray());
     }
