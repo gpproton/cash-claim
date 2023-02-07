b3f0ddeee4027192163cf2ddf414e4a359ddb80e
radioActive DROID <me@godwin.dev>
2023-02-07 17:39:01 +0100

    update and added additional DTo for company transfer

--- src/Common/Dtos/TransferRequestResponse.cs
+++ src/Common/Dtos/TransferRequestResponse.cs
@@ -5,4 +5,7 @@ namespace XClaim.Common.Dtos;
 public class TransferRequestResponse : BaseResponse {
     public UserResponse? User { get; set; }
     public Guid? UserId { get; set; }
+    public CompanyResponse? Company { get; set; }
+    public Guid? CompanyId { get; set; }
+    public bool Completed { get; set; }
 }
