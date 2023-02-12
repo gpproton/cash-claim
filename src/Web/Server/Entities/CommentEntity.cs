ec166de6dcc8cbadb8ed20078d0e49ea86b4335b
radioActive DROID <me@godwin.dev>
2023-02-12 21:27:22 +0100

    Improvements to comment and file entities

--- src/Web/Server/Entities/CommentEntity.cs
+++ src/Web/Server/Entities/CommentEntity.cs
@@ -4,14 +4,12 @@ using XClaim.Common.Base;
 namespace XClaim.Web.Server.Entities;
 
 public sealed class CommentEntity : BaseEntity {
+    public UserEntity? Owner { get; set; }
+    public Guid? OwnerId { get; set; }
     public ClaimEntity? Claim { get; set; }
     public Guid? ClaimId { get; set; }
     public PaymentEntity? Payment { get; set; }
     public Guid? PaymentId { get; set; }
-    [Required]
-    public UserEntity? Owner { get; set; }
-    public Guid? OwnerId { get; set; }
-    [Required]
     [MaxLength(1024)]
     public string? Content { get; set; }
 }
--- src/Web/Server/Entities/FileEntity.cs
+++ src/Web/Server/Entities/FileEntity.cs
@@ -12,4 +12,9 @@ public sealed class FileEntity : BaseEntity {
     [MaxLength(8)]
     public string? Extension { get; set; }
     public UserEntity? Owner { get; set; }
+    public Guid? OwnerId { get; set; }
+    public ClaimEntity? Claim { get; set; }
+    public Guid? ClaimId { get; set; }
+    public PaymentEntity? Payment { get; set; }
+    public Guid? PaymentId { get; set; }
 }
