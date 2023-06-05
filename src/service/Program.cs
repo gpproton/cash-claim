9bdc6535a226d27fb539ffbbf6da0b7dc5ac41c9
radioActive DROID <me@godwin.dev>
2023-06-05 04:27:13 +0100

    enabled pre-rendering and webcil format

--- src/service/Program.cs
+++ src/service/Program.cs
@@ -6,16 +6,16 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment()) {
     app.UseWebAssemblyDebugging();
-} else {
+}
+else {
     app.UseExceptionHandler("/Error");
 }
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
-
 app.UseRouting();
 app.MapRazorPages();
 app.MapControllers();
-app.MapFallbackToFile("index.html");
+app.MapFallbackToPage("/_Host");
 
 app.Run();
