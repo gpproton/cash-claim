969be59ee497bc2116a2a61aa6331261b8c67a11
radioActive DROID <me@godwin.dev>
2023-06-06 05:03:45 +0100

    setup migration on service

---
+++ src/service/Data/DatabaseExtensions.cs
@@ -0,0 +1,42 @@
+// Copyright 2023 - 2023 Godwin peter .O (me@godwin.dev)
+//
+// Licensed under the MIT License;
+// you may not use this file except in compliance with the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.EntityFrameworkCore;
+using static XClaim.Service.Data.Provider;
+
+namespace XClaim.Service.Data;
+
+public static class DatabaseExtensions {
+    public static IServiceCollection RegisterDataContext(this IServiceCollection services) {
+        var sp = services.BuildServiceProvider();
+        var config = sp.GetService<IConfiguration>();
+
+        services.AddDbContext<ServiceContext>(options => {
+                options.UseSnakeCaseNamingConvention();
+                var provider = config!.GetValue("provider", Sqlite.Name);
+
+                if (provider == Sqlite.Name) {
+                    options.UseSqlite(config!.GetConnectionString(Sqlite.Name)!, x => x.MigrationsAssembly(Sqlite.Assembly).UseRelationalNulls());
+                }
+
+                if (provider == Postgres.Name) {
+                    options.UseNpgsql(config!.GetConnectionString(Postgres.Name)!,  x => x.MigrationsAssembly(Postgres.Assembly).UseRelationalNulls());
+                }
+
+                if (provider == Mysql.Name) {
+                    var connectionString = config!.GetConnectionString(Postgres.Name)!;
+                    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), x => x.MigrationsAssembly(Mysql.Assembly).UseRelationalNulls());
+                }
+            }
+        );
+
+        return services;
+    }
+}
--- src/service/Program.cs
+++ src/service/Program.cs
@@ -1,21 +1,48 @@
+using Axolotl.AspNet;
+using Axolotl.EFCore;
+using DotNetEd.CoreAdmin;
+using XClaim.Service.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => { });
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.RegisterFeatures(typeof(Program).Assembly);
+builder.Services.RegisterGenericServices();
+
+builder.Services.RegisterDataContext();
+builder.Services.RegisterGenericRepositories(typeof(GenericRepository<>));
+builder.Services.AddHostedService<MigrationService>();
+builder.Services.AddCoreAdmin(new CoreAdminOptions {
+    Title = "X-Claim",
+    ShowPageSizes = true,
+    PageSizes = new Dictionary<int, string>() {
+        { 25, "25"},
+        { 100, "100"},
+        { 0, "All"}
+    },
+    IgnoreEntityTypes = new List<Type> { }
+});
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment()) {
     app.UseWebAssemblyDebugging();
-}
-else {
+} else {
     app.UseExceptionHandler("/Error");
 }
 
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.MapDefaultControllerRoute();
 app.UseRouting();
+app.UseStaticFiles();
+app.UseAuthorization();
+app.UseBlazorFrameworkFiles();
 app.MapRazorPages();
 app.MapControllers();
+app.UseCoreAdminCustomUrl("admin");
+app.UseCoreAdminCustomAuth((serviceProvider) => Task.FromResult(true));
 app.MapFallbackToPage("/_Host");
 
 app.Run();
