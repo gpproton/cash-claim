366eba1047532f4f45e331c6da6b23582ff6bdd7
radioActive DROID <me@godwin.dev>
2023-06-09 23:59:38 +0100

    some random code port

--- src/web/client/Program.cs
+++ src/web/client/Program.cs
@@ -1,9 +1,11 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using XClaim.Web.Client;
+using XClaim.Web.Components.Extensions;
+using XClaim.Web.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.UseSharedExtensions();
+builder.Services.UseComponentsExtensions();
 
 await builder.Build().RunAsync();
