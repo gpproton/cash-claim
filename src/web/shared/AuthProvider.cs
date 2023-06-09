2225b9c5beb5c87aca5cdd6eb898d69740cfd9e9
radioActive DROID <me@godwin.dev>
2023-06-09 23:55:33 +0100

    more code port

---
+++ src/web/shared/AuthProvider.cs
@@ -0,0 +1,70 @@
+using System.Security.Claims;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using XClaim.Common.Responses;
+
+namespace XClaim.Web.Shared;
+
+public class AuthProvider : AuthenticationStateProvider {
+    private readonly ISessionStorageService _sessionStorage;
+    private readonly ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+    public AuthProvider(ISessionStorageService sessionStorage) {
+        _sessionStorage = sessionStorage;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync() {
+        try {
+            var userSession = await _sessionStorage.GetAsync<AuthResponse>(WebConst.SessionKey);
+            if (userSession == null)
+                return await Task.FromResult(new AuthenticationState(_anonymous));
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> {
+                new (ClaimTypes.Name, userSession.UserName),
+                new (ClaimTypes.Role, userSession.Role)
+            }, "SessionAuth"));
+
+            return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+        }
+        catch {
+            return await Task.FromResult(new AuthenticationState(_anonymous));
+        }
+    }
+
+    public async Task UpdateAuthenticationState(AuthResponse? userSession) {
+        ClaimsPrincipal claimsPrincipal;
+
+        if (userSession != null) {
+            claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                    new (ClaimTypes.Name, userSession.UserName),
+                    new (ClaimTypes.Role, userSession.Role)
+                }));
+            userSession.ExpiryTimeStamp = DateTime.Now.AddSeconds(userSession.ExpiresIn);
+            await _sessionStorage.SaveAsync(WebConst.SessionKey, userSession);
+        }
+        else {
+            claimsPrincipal = _anonymous;
+            await _sessionStorage.RemoveItemAsync(WebConst.SessionKey);
+        }
+
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+    }
+
+    public async Task RefreshAuthenticationState(AuthResponse userSession) {
+        await _sessionStorage.SaveAsync(WebConst.SessionKey, userSession);
+    }
+
+    public async Task ClearAuthInfo() => await this.UpdateAuthenticationState(null);
+
+    public async Task<string?> GetToken() {
+        var result = string.Empty;
+        try {
+            var userSession = await _sessionStorage.GetAsync<AuthResponse>(WebConst.SessionKey);
+            if (userSession != null && DateTime.Now < userSession.ExpiryTimeStamp)
+                result = userSession.Token;
+        } catch {
+            // ignored
+        }
+        return result;
+    }
+}
