2c3630f771cf2ec5b6174b5edbd2e06935cab043
radioActive DROID <me@godwin.dev>
2023-06-09 23:55:13 +0100

    start web shared project update

---
+++ src/web/shared/WebConst.cs
@@ -0,0 +1,16 @@
+using MudBlazor;
+
+namespace XClaim.Web.Shared;
+
+public static class WebConst {
+    public const string RootApi = "/api/v1";
+    public const string RootUri = "https://localhost:7001";
+    public const string ApiAuth = "auth/sign-in";
+    public const string AppHome = "app/overview";
+    public const string AppAuth = "app/auth";
+    public const string AppRegister = "app/registration";
+    public const string SessionKey = "UserSession";
+    public static readonly DateRange AppDateRange = new DateRange(DateTime.Now.AddDays(-7), DateTime.Now);
+    public static readonly int[] AppPaged = { 25, 100, 250, 2000 };
+    public const string TableHeight = "calc(100vh - 224px)";
+}
