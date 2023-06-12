f6afcb4775ff6d7189f631a626c15dbde41ae6aa
radioActive DROID <me@godwin.dev>
2023-06-12 22:15:08 +0100

    update service project

--- src/service/Program.cs
+++ src/service/Program.cs
@@ -1,6 +1,17 @@
+// Copyright 2023 - 2023 Godwin peter .O (me@godwin.dev)
+//
+// Licensed under the MIT License;
+// you may not use this file except in compliance with the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using XClaim.Service.Extensions;
+using XClaim.Web.Components.Extensions;
 using XClaim.Web.Shared;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -17,7 +28,9 @@ builder.Services.AddSingleton<HttpClient>(sp => {
     string baseAddress = addressFeature!.Addresses.First();
     return new HttpClient { BaseAddress = new Uri(baseAddress) };
 });
+
 builder.Services.RegisterSharedBlazorServices();
+builder.Services.RegisterAppState();
 
 WebApplication app = builder.Build();
 
