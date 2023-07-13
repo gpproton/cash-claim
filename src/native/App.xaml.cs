4e52dfc262e103d5d86eb00fca3fc903d94dc837
radioActive DROID <me@godwin.dev>
2023-07-13 05:00:24 +0100

    Added native project

---
+++ src/native/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace XClaim.App.Native;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ src/native/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace XClaim.App.Native;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
