c88bb30cef08a74eb7a78f07f7dde62893e59d80
radioActive DROID <me@godwin.dev>
2023-11-18 00:53:12 +0100

    additional entity adjustments

--- src/service/Features/AccountModule/ProfileFeature.cs
+++ src/service/Features/AccountModule/ProfileFeature.cs
@@ -17,10 +17,10 @@ namespace XClaim.Service.Features.AccountModule;
 
 public class ProfileFeature : GenericFeature<ProfileFeature> {
     public override IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints) {
-        IEndpointRouteBuilder? group = SetupGroup<ProfileFeature, UserEntity, User, Guid>(endpoints,
+        IEndpointRouteBuilder? group = SetupGroup<ProfileFeature, ProfileEntity, User, Guid>(endpoints,
             new FeatureState(new List<RouteState> { }, Name: "Profile", Path: "account/profile"));
 
-        AddGetById<UserEntity, User, Guid>(new RouteState(RouteType.Any, Name: "GetProfileById"));
+        AddGetById<ProfileEntity, User, Guid>(new RouteState(RouteType.Any, Name: "GetProfileById"));
 
         return group;
     }
